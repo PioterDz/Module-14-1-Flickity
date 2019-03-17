c7dd7d8a22087161fe191e4e75307d2856c1b8b5
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-03-17 18:17:44 +0100

    make nothing special

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
+for(var i = 0; i < properties.length; i++){
+  console.log(properties);
+  listItems += Mustache.render(templateItem, properties[i]);
+}
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  wrapAround: true
 });
 
 // element argument can be a selector string
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
-  // options
+
 });
 
 var flkty = new Flickity( '.carousel', {
@@ -18,8 +25,11 @@ var flkty = new Flickity( '.carousel', {
 var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, 0.16 ) );
+  progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
+
+
+
 
 
