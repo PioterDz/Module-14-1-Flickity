f0f4ff79371dd7da2e92485d16b85c4f9ce6e8ed
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-05 18:03:42 +0200

    make scroll and select button functions

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 var elem = document.querySelector('.main-carousel');
 var progressBar = document.querySelector('.progress-bar');
-var returnButton = document.getElementById('button-return');
+var returnButton = document.getElementById('button');
 
 var flkty = new Flickity( elem, {
   // options
   hash: true,
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  wrapAround: true
 });
 
-var flkty = new Flickity('.carousel');
+flkty.on('scroll', function(progress) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
 
-returnButton.addEventListener('click', flkty.select( 0, true, true ));
-
-flkty.on( 'scroll', scroll(1));
-
-
-
+returnButton.addEventListener('click', function(event) {
+  var index = 0;
+  flkty.select(index, true, true);
+});
 
 
 
