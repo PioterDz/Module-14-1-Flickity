6fc73a7fbb8708add7785044783e7ea9c1b01e37
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-05 18:25:31 +0200

    before merge

--- js/script.js
+++ js/script.js
@@ -3,30 +3,26 @@ for(var i = 0; i < properties.length; i++){
   listItems += Mustache.render(templateItem, properties[i]);
 }
 
-
 var elem = document.querySelector('.main-carousel');
+var progressBar = document.querySelector('.progress-bar');
+var returnButton = document.getElementById('button');
+
 var flkty = new Flickity( elem, {
   // options
+  hash: true,
   cellAlign: 'left',
   contain: true,
   wrapAround: true
 });
 
-// element argument can be a selector string
-//   for an individual element
-var flkty = new Flickity( '.main-carousel', {
-
-});
-
-var flkty = new Flickity( '.carousel', {
-  hash: true,
-});
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
+flkty.on('scroll', function(progress) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
+});
+
+returnButton.addEventListener('click', function(event) {
+  var index = 0;
+  flkty.select(index, true, true);
 });
 
 
